57fe0e78a0261509f3c6d906f4a1d9ccd768ed6e
James Burnell <jamesburnell@Jamess-MacBook-Pro.local>
2016-03-05 18:49:25 -0800

    gsap added

--- butterfli-webview/client/components/Approved.jsx
+++ butterfli-webview/client/components/Approved.jsx
@@ -1,12 +1,21 @@
 var React = require('react');
+// var gsap = require('gsap');
+
+// var TweenMax = gsap.TweenMax;
 
 var Approved = React.createClass({
+
+	componentDidMount(){
+		var node = this.getDOMNode();
+
+		TweenMax.from(node, 5, {y:30});
+	},
 
 	_renderContent(){
 		if(this.props.approvedPosts){
 			return this.props.approvedPosts.map( (element) => {
 				return ( 
-					<div style={{textAlign: 'center'}} className="uk-width-1-3 uk-panel uk-panel-box">
+					<div style={{textAlign: 'center'}} className="uk-width-1-3 uk-panel uk-panel-box stagger">
 						<img style={{height: 200}} src={element.og_source}></img>
 						<p>{element.title}</p>
 						<a onClick={ () => {this.props.postApproval(this.props.currentDash[0].id, element.id, 'toggle_disapprove') } } className="uk-button uk-width-1-2">Disapprove</a>
