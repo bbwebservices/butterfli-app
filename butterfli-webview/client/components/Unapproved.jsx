103bf031afc4b92e99fa705f183b4969d03449c1
James Burnell <jamesburnell@Jamess-MacBook-Pro.local>
2016-03-10 18:21:37 -0800

    tabs layout fixed

--- butterfli-webview/client/components/Unapproved.jsx
+++ butterfli-webview/client/components/Unapproved.jsx
@@ -5,6 +5,39 @@ var UnapprovedListItem = require('./scrape_components/unapprovedListItem.jsx')
 
 var Unapproved = React.createClass({
 
+	getInitialState(){
+		return {
+			twitterTerm: '',
+			giphyTerm: '',
+			redditTerm: '',
+			tumblrTerm: ''
+		}
+	},
+
+	updateTwitterTerm(){
+		this.setState({
+			twitterTerm: this.refs.twitterTerm.value
+		})
+	},
+	
+	updateGiphyTerm(){
+		this.setState({
+			giphyTerm: this.refs.giphyTerm.value
+		})
+	},
+
+	updateRedditTerm(){
+		this.setState({
+			redditTerm: this.refs.redditTerm.value
+		})
+	},
+
+	updateTumblrTerm(){
+		this.setState({
+			tumblrTerm: this.refs.tumblrTerm.value
+		})
+	},
+
 	animateListItems(){
 		TweenMax.staggerFrom('.stagger', 0.3, {y:30, x: 10, opacity: 0}, 0.05);
 	},
@@ -33,9 +66,37 @@ var Unapproved = React.createClass({
 	render(){
 		return (
 			<div className="uk-grid uk-margin-top ">
-					
-				{this._renderContent()}
-						
+				<div className="uk-width-1-4">
+					<div className="uk-panel uk-panel-box uk-panel-box-primary">
+						<form className='uk-form'>
+							<input ref="twitterTerm" onChange={ this.updateTwitterTerm } type='text' className="uk-width-1-1" />
+							<a onClick={ ()=>{this.props.picScrape(this.props.currentDash[0].id, 'twitter', this.state.twitterTerm)} } className='uk-button uk-button-large uk-width-1-1'>Search Twitter</a>
+						</form>
+					</div>
+					<div className="uk-panel uk-panel-box uk-panel-box-primary">
+						<form className='uk-form'>
+							<input ref="giphyTerm" onChange={ this.updateGiphyTerm } type='text' className="uk-width-1-1" />
+							<a onClick={ ()=>{this.props.picScrape(this.props.currentDash[0].id, 'giphy', this.state.giphyTerm)} } className='uk-button uk-button-large uk-width-1-1'>Search Giphy</a>
+						</form>
+					</div>
+					<div className="uk-panel uk-panel-box uk-panel-box-primary">
+						<form className='uk-form'>
+							<input ref="redditTerm" onChange={ this.updateRedditTerm } type='text' className="uk-width-1-1" />
+							<a onClick={ ()=>{this.props.picScrape(this.props.currentDash[0].id, 'reddit', this.state.redditTerm)} } className='uk-button uk-button-large uk-width-1-1'>Search Reddit</a>
+						</form>
+					</div>
+					<div className="uk-panel uk-panel-box uk-panel-box-primary">
+						<form className='uk-form'>
+							<input ref="tumblrTerm" onChange={ this.updateTumblrTerm } type='text' className="uk-width-1-1" />
+							<a onClick={ ()=>{this.props.picScrape(this.props.currentDash[0].id, 'tumblr', this.state.tumblrTerm)} } className='uk-button uk-button-large uk-width-1-1'>Search Tumblr</a>
+						</form>
+					</div>
+				</div>
+				<div className="uk-width-3-4">
+					<div className="uk-grid">
+						{this._renderContent()}
+					</div>
+				</div>	
 			</div>
 		)
 	}
