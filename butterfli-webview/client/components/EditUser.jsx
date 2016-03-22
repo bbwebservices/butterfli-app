64378ac1ad3722588386c5db0c94b770adb390c4
James Burnell <jamesaburnell@gmail.com>
2016-03-22 10:13:59 -0700

    user info client side

--- butterfli-webview/client/components/EditUser.jsx
+++ butterfli-webview/client/components/EditUser.jsx
@@ -3,6 +3,15 @@ var Navbar = require('./Navbar.jsx')
 
 var EditAccount = React.createClass({
 
+	animateListItems(){
+		TweenMax.staggerFrom('.stagger', 0.3, {y:30, x: 10, opacity: 0}, 0.02);
+		TweenMax.from('.fadeIn', 0.3, {scale: 1.04, opacity: 0})
+	},
+
+	componentDidMount(){
+		this.animateListItems();
+	},
+
 	render(){
 		return(
 			<div>
@@ -10,7 +19,27 @@ var EditAccount = React.createClass({
 				<div className="uk-container uk-container-center uk-margin-top fadeIn">
 					<div className="uk-grid">	
 						<div style={{marginTop: 100}} className="uk-width-medium-1-4 uk-row-first stagger column1">
-							<h1>Edit User Page</h1>
+							<h1>Edit User Info</h1>
+							<div className="uk-panel uk-panel-box">	
+									<div className="uk-form-row">
+										<label className="uk-form-label">New Username</label>
+										<div className="uk-form-controls">
+											<input type="text" className="uk-width-2-3" />
+										</div>
+									</div>
+									<div className="uk-form-row">
+										<label className="uk-form-label">New Password</label>
+										<div className="uk-form-controls">
+											<input type="text" className="uk-width-2-3" />
+										</div>
+									</div>
+									<div className="uk-form-row">
+										<label className="uk-form-label">New Password Confirmation</label>
+										<div className="uk-form-controls">
+											<input type="text" className="uk-width-2-3" />
+										</div>
+									</div>
+								</div>
 						</div>
 					</div>
 				</div>	
