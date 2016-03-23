4c1d628d69eb0b92af7a1241115e7467605b0eae
hathbanger <hathbanger.ah@gmail.com>
2016-03-23 13:48:53 -0600

    specified twitter search

--- butterfli-api/app/models/dash.rb
+++ butterfli-api/app/models/dash.rb
@@ -89,7 +89,7 @@ class Dash < ActiveRecord::Base
 		pic_limit = 0
 		pic_fail = 0
 		count = 0
-		t.search(search_var, options = {}).collect do |tweet|
+		t.search(search_var, options = {result_type: 'popular', max_id: '',  filter: 'twimg'}).collect do |tweet|
 			puts 'tweet', tweet.to_json
 			puts 'index', count
 			count += 1
