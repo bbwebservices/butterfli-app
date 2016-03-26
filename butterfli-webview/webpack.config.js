e865512ca57d4cd586b7b53bfe2c30806e73f9e3
James Burnell <jamesaburnell@gmail.com>
2016-03-26 11:51:10 -0700

    webpack started

--- butterfli-webview/webpack.config.js
+++ butterfli-webview/webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
 		loaders: [
 			{
 				test: /\.jsx?$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel-core',
+				exclude: /node_modules/,
+				loader: 'babel-loader',
 				query: {
 					presets: ['es2015', 'react']
 				}
