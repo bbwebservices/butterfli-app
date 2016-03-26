e13bcfaf1b84c8c65ccc77094e37296e92550ea6
James Burnell <jamesaburnell@gmail.com>
2016-03-26 13:06:46 -0700

    request almost working

--- butterfli-webview/webpack.config.js
+++ butterfli-webview/webpack.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+var StringReplacePlugin = require("string-replace-webpack-plugin");
+
+var config = {
 	entry: './client/main.jsx',
 	output: {
 		filename: 'bundle.js',
@@ -13,7 +15,42 @@ module.exports = {
 				query: {
 					presets: ['es2015', 'react']
 				}
+			},
+			{ 
+				test:  /\.json$/, 
+				loader: 'json-loader' 
+			},
+			{
+				test: /\.js?$/,
+				include: /node_modules\/request/,
+				loader: StringReplacePlugin.replace({ // from the 'string-replace-webpack-plugin'
+			        replacements: [ {
+			          pattern: /define\.amd/ig,
+			          replacement: function(match, p1, offset, string) {
+			            return false;
+			          }
+			        }]
+			    })
 			}
-		]
+		],
+
+
+	},
+	node: {
+		console: 'empty',
+		fs: 'empty',
+		net: 'empty',
+		tls: 'empty'
+	},
+	externals: {
+		request: false
 	}
 }
+
+// config.node = {
+// 	console: 'empty',
+// 	fs: 'empty',
+// 	net: 'empty',
+// 	tls: 'empty'
+// }
+module.exports = config;
